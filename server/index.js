f7789fa6edaac8006234dbb8c1e070091c4b9514
victor schmoegel <victorschmoegel@gmail.com>
2023-11-29 16:38:09 -0300

    connected to de database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
+});
 
 
 app.listen(3001, () => {
