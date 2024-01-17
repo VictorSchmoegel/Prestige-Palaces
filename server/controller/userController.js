f82a5601cd07b19c0a21a0eebb166e670d780653
victor schmoegel <victorschmoegel@gmail.com>
2024-01-17 15:08:20 -0300

    created user update route

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,9 +1,44 @@
+const errorHandler = require('../middlewares/errorHandler');
+const bcryptjs = require('bcryptjs');
+const User = require('../model/userModel.js');
+
 const createUser = (req, res) => {
   res.json({
     message: 'User created',
   });
 };
 
+const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    // eslint-disable-next-line no-unused-vars
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   createUser,
+  updateUser,
 };
