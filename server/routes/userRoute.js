64d1cd80a5ae1ad0e3806e13c41f200ef6a754e5
victor schmoegel <victorschmoegel@gmail.com>
2024-01-18 13:33:11 -0300

    created a api route for listings

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { updateUser, deleteUser } = require('../controller/userController.js');
+const { updateUser, deleteUser, getUserListings } = require('../controller/userController.js');
 const { verifyToken } = require('../middlewares/verifyUser.js');
 
 router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 module.exports = router;
