b4bd8215956d88a58be75244187e05855f699c49
victor schmoegel <victorschmoegel@gmail.com>
2024-01-19 11:39:57 -0300

    created UI for search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,96 @@
 export default function Search() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>Search</div>
+    <main className='flex flex-col md:flex-row'>
+      <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
+        <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
+          <div className='flex items-center gap-2'>
+            <label className='whitespace-nowrap font-semibold' htmlFor='search'> Search Term:
+              <input
+                className='border rounded-lg p-3 w-full'
+                type="text"
+                placeholder="Search..."
+                id='search'
+              />
+            </label>
+          </div>
+          <div className='flex gap-2 flex-wrap items-center'>
+            <label className='font-semibold'>Type:</label>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='all'
+                className='w-5'
+              />
+              <span>Rent & Sale</span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='all'
+                className='w-5'
+              />
+              <span>Rent</span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='all'
+                className='w-5'
+              />
+              <span>Sale</span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='all'
+                className='w-5'
+              />
+              <span>Offer</span>
+            </div>
+          </div>
+          <div className='flex gap-2 flex-wrap items-center'>
+            <label className='font-semibold'>Amenities:</label>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='parking'
+                className='w-5'
+              />
+              <span>Parking</span>
+            </div>
+            <div className='flex gap-2'>
+              <input
+                type='checkbox'
+                id='furnished'
+                className='w-5'
+              />
+              <span>Furnished</span>
+            </div>
+          </div>
+          <div className='flex items-center gap-2'>
+            <label className='font-semibold'>Sort:</label>
+            <select
+              defaultValue={'created_at_desc'}
+              id='sort_order'
+              className='border rounded-lg p-3'
+            >
+              <option value='regularPrice_desc'>Price high to low</option>
+              <option value='regularPrice_asc'>Price low to hight</option>
+              <option value='createdAt_desc'>Latest</option>
+              <option value='createdAt_asc'>Oldest</option>
+            </select>
+          </div>
+          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+            Search
+          </button>
+        </form>
+      </div>
+      <div>
+        <h2>List results:</h2>
+      </div>
+    </main>
   )
 }
